f0befb89621f49374b36e6d1f1ee98c128040685
tkdans7589 <32192086+tkdans7589@users.noreply.github.com>
2021-07-07 18:20:39 +0900

    Naive Bayes 과제
    13장 TP의 SpamAssassin public corpus 를 이용하여
    Training: Test = 8:2 비율로 나누어 다음을 구하시오
    
    1) 혼동행렬
    2)  spammiest workds (10개)
          hammiest workds (10개)

---
+++ Naive Bayes/Naive Bayes HW.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 27 20:16:40 2021
+
+@author: tkdan
+"""
+
+from typing import Set
+import re
+
+def tokenize(text: str) -> Set[str]:
+    text = text.lower()                         # Convert to lowercase,
+    all_words = re.findall("[a-z0-9']+", text)  # extract the words, and
+    return set(all_words)                       # remove duplicates.
+
+from typing import NamedTuple
+
+class Message(NamedTuple):
+    text: str
+    is_spam: bool
+
+from typing import List, Tuple, Dict, Iterable
+import math
+from collections import defaultdict
+
+class NaiveBayesClassifier:
+    def __init__(self, k: float = 0.5) -> None:
+        self.k = k  # smoothing factor
+
+        self.tokens: Set[str] = set()
+        self.token_spam_counts: Dict[str, int] = defaultdict(int)
+        self.token_ham_counts: Dict[str, int] = defaultdict(int)
+        self.spam_messages = self.ham_messages = 0
+    
+    def train(self, messages: Iterable[Message]) -> None:
+        for message in messages:
+            # Increment message counts
+            if message.is_spam:
+                self.spam_messages += 1
+            else:
+                self.ham_messages += 1
+
+            # Increment word counts
+            for token in tokenize(message.text):
+                self.tokens.add(token)
+                if message.is_spam:
+                    self.token_spam_counts[token] += 1
+                else:
+                    self.token_ham_counts[token] += 1
+
+    def _probabilities(self, token: str) -> Tuple[float, float]:
+        """returns P(token | spam) and P(token | not spam)"""
+        spam = self.token_spam_counts[token]
+        ham = self.token_ham_counts[token]
+
+        p_token_spam = (spam + self.k) / (self.spam_messages + 2 * self.k)
+        p_token_ham = (ham + self.k) / (self.ham_messages + 2 * self.k)
+
+        return p_token_spam, p_token_ham
+
+    def predict(self, text: str) -> float:
+        text_tokens = tokenize(text)
+        log_prob_if_spam = log_prob_if_ham = 0.0
+
+        # Iterate through each word in our vocabulary.
+        for token in self.tokens:
+            prob_if_spam, prob_if_ham = self._probabilities(token)
+
+            # If *token* appears in the message,
+            # add the log probability of seeing it;
+            if token in text_tokens:
+                log_prob_if_spam += math.log(prob_if_spam)
+                log_prob_if_ham += math.log(prob_if_ham)
+
+            # otherwise add the log probability of _not_ seeing it
+            # which is log(1 - probability of seeing it)
+            else:
+                log_prob_if_spam += math.log(1.0 - prob_if_spam)
+                log_prob_if_ham += math.log(1.0 - prob_if_ham)
+
+        prob_if_spam = math.exp(log_prob_if_spam)
+        prob_if_ham = math.exp(log_prob_if_ham)
+        return prob_if_spam / (prob_if_spam + prob_if_ham)
+
+
+from io import BytesIO
+import requests
+import tarfile
+
+BASE_URL = "https://spamassassin.apache.org/old/publiccorpus/"
+FILES=['20021010_easy_ham.tar.bz2','20021010_hard_ham.tar.bz2','20021010_spam.tar.bz2']
+
+OUTPUT_DIR = 'spam_data'
+
+for filename in FILES:
+    content=requests.get(f"{BASE_URL}/{filename}").content
+    fin=BytesIO(content)
+    with tarfile.open(fileobj=fin, mode='r:bz2') as tf:
+        tf.extractall(OUTPUT_DIR)
+
+
+import glob
+    
+# modify the path to wherever you've put the files
+path = 'spam_data/*/*'
+
+data: List[Message] = []
+
+# glob.glob returns every filename that matches the wildcarded path
+for filename in glob.glob(path):
+    is_spam = "ham" not in filename
+
+    # There are some garbage characters in the emails, the errors='ignore'
+    # skips them instead of raising an exception.
+    with open(filename, errors='ignore') as email_file:
+        for line in email_file:
+            if line.startswith("Subject:"):
+                subject = line.lstrip("Subject: ")
+                data.append(Message(subject, is_spam))
+                break  # done with this file
+
+import random
+from scratch.machine_learning import split_data
+
+random.seed(0)      # just so you get the same answers as me
+train_messages, test_messages = split_data(data, 0.8)
+
+model = NaiveBayesClassifier()
+model.train(train_messages)
+
+from collections import Counter
+
+predictions = [(message, model.predict(message.text))
+               for message in test_messages]
+
+# Assume that spam_probability > 0.5 corresponds to spam prediction
+# and count the combinations of (actual is_spam, predicted is_spam)
+confusion_matrix = Counter((message.is_spam, spam_probability > 0.5)
+                           for message, spam_probability in predictions)
+
+print(confusion_matrix)
+
+def p_spam_given_token(token: str, model: NaiveBayesClassifier) -> float:
+    # We probably shouldn't call private methods, but it's for a good cause.
+    prob_if_spam, prob_if_ham = model._probabilities(token)
+
+    return prob_if_spam / (prob_if_spam + prob_if_ham)
+
+words = sorted(model.tokens, key=lambda t: p_spam_given_token(t, model))
+
+print("spammiest_words", words[-10:])
+print("hammiest_words", words[:10])
